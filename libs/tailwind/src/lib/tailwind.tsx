fe984cc58ebd825c888734c6d06d4706f715d4a4
Jason Ruesch <jruesch@degreed.com>
2024-01-26 23:17:09 -0500

    feat(tailwind): update welcome message

--- libs/tailwind/src/lib/tailwind.tsx
+++ libs/tailwind/src/lib/tailwind.tsx
@@ -6,7 +6,7 @@ export interface TailwindProps {}
 export function Tailwind(props: TailwindProps) {
   return (
     <div className={styles['container']}>
-      <h1>Welcome to Tailwind!</h1>
+      <h1>Welcome to my custom Tailwind CSS configuration!</h1>
     </div>
   );
 }
