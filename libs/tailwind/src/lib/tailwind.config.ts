243d7c076158e3b1eee3659d626eb430150d0e23
Jason Ruesch <jruesch@degreed.com>
2024-01-29 07:13:36 -0500

    fix(tailwind): swap bg-gradient-X and bg-gradient-X-sm

--- libs/tailwind/src/lib/tailwind.config.ts
+++ libs/tailwind/src/lib/tailwind.config.ts
@@ -41,10 +41,10 @@ export const tailwindPreset = {
       },
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
       backgroundImage: ({ theme }: any) => ({
-        'gradient-500': `linear-gradient(145deg, ${theme('colors.primary-500')} 45%, ${theme('colors.secondary-500')}, ${theme('colors.white')}, ${theme('colors.secondary-500')}, ${theme('colors.primary-500')} 55%)`,
-        'gradient-400': `linear-gradient(145deg, ${theme('colors.primary-400')} 45%, ${theme('colors.secondary-400')}, ${theme('colors.black')}, ${theme('colors.secondary-400')}, ${theme('colors.primary-400')} 55%)`,
-        'gradient-500-sm': `linear-gradient(145deg, ${theme('colors.primary-500')} 45%, ${theme('colors.secondary-500')} 49%, ${theme('colors.secondary-500')} 51%, ${theme('colors.primary-500')} 55%)`,
-        'gradient-400-sm': `linear-gradient(145deg, ${theme('colors.primary-400')} 45%, ${theme('colors.secondary-400')} 49%, ${theme('colors.secondary-400')} 51%, ${theme('colors.primary-400')} 55%)`,
+        'gradient-500-sm': `linear-gradient(145deg, ${theme('colors.primary-500')} 45%, ${theme('colors.secondary-500')}, ${theme('colors.white')}, ${theme('colors.secondary-500')}, ${theme('colors.primary-500')} 55%)`,
+        'gradient-400-sm': `linear-gradient(145deg, ${theme('colors.primary-400')} 45%, ${theme('colors.secondary-400')}, ${theme('colors.black')}, ${theme('colors.secondary-400')}, ${theme('colors.primary-400')} 55%)`,
+        'gradient-500': `linear-gradient(145deg, ${theme('colors.primary-500')} 45%, ${theme('colors.secondary-500')} 49%, ${theme('colors.secondary-500')} 51%, ${theme('colors.primary-500')} 55%)`,
+        'gradient-400': `linear-gradient(145deg, ${theme('colors.primary-400')} 45%, ${theme('colors.secondary-400')} 49%, ${theme('colors.secondary-400')} 51%, ${theme('colors.primary-400')} 55%)`,
       }),
     },
   },
@@ -90,7 +90,7 @@ export const tailwindPreset = {
 
       addComponents({
         '.gradient-heading': {
-          '@apply py-4 text-center text-xl font-normal uppercase !leading-cap sm:text-5xl lg:max-w-screen-lg lg:text-6xl bg-gradient-500-sm md:bg-gradient-500 dark:bg-gradient-400-sm dark:md:bg-gradient-400 bg-300% animate-gradient bg-clip-text text-transparent scale-110 sm:scale-100 motion-reduce:animate-none':
+          '@apply py-4 text-center text-xl font-normal uppercase !leading-cap sm:text-5xl lg:max-w-screen-lg lg:text-6xl bg-gradient-500 sm:bg-gradient-500-sm dark:bg-gradient-400 dark:sm:bg-gradient-400-sm bg-300% animate-gradient bg-clip-text text-transparent scale-110 sm:scale-100 motion-reduce:animate-none':
             '',
         },
       });
