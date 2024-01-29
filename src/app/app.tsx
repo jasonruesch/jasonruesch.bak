83e39c61bc240e51f0328cc55e8ca37bd52dfb20
Jason Ruesch <jruesch@degreed.com>
2024-01-29 07:43:27 -0500

    fix(jasonruesch): add scroll restoration

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,7 +1,13 @@
 import { Layout } from '@jasonruesch/ui';
+import { ScrollRestoration } from 'react-router-dom';
 
 export function App() {
-  return <Layout />;
+  return (
+    <>
+      <Layout />
+      <ScrollRestoration />
+    </>
+  );
 }
 
 export default App;
